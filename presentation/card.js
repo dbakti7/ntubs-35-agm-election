0b17ca8bdbda9b38e860ab0cf73a11ba354bc1f0
kenrick95 <kenrick95@gmail.com>
2017-08-29 22:43:56 +0800

    Update card colors

--- presentation/card.js
+++ presentation/card.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react'
+
+const niceColorMapping = {
+  green: '#2ECC40',
+  blue: '#0074D9',
+  yellow: '#FFDC00',
+  red: '#FF4136',
+  black: '#111111'
+}
 
 function Card({ color }) {
+  const fillColor = niceColorMapping[color] || color
   return (
     <svg width="90" height="120" viewBox="0 0 90 120">
-      <rect fill={color} x="0" y="0" width="90" height="120" />
+      <rect fill={fillColor} x="0" y="0" width="90" height="120" />
     </svg>
-  );
+  )
 }
-export default Card;
+export default Card
